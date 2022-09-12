8f9cb2290e8b27ef1d65aaff13faf28b11f56143
spaisen <spd7abel@gmail.com>
2022-09-12 10:06:37 +0545

    yay

---
+++ age.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+age = abs(age)
+#proper use of if-elif-else block
+if 0=<age<=10:
+    print("Since you're a kid, your  movie ticket is Rs. 100.")
+elif 10<age<=20:
+    print("Your movie ticket is Rs. 200.")
+else:
+    print("Your movie ticket is Rs. 300")
+    
+    
